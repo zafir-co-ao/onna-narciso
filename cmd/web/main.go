e3a81ef9da2f3cfc787af8a2c6fbc496a12db2b2
jocaetano1 <josan5368@gmail.com>
2024-10-29 18:25:18 +0100

    feat: add bus in use cases

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,19 +6,21 @@ import (
 	"github.com/zafir-co-ao/onna-narciso/internal/scheduling"
 	"github.com/zafir-co-ao/onna-narciso/internal/scheduling/adapters/inmem"
 	"github.com/zafir-co-ao/onna-narciso/internal/scheduling/tests/stubs"
+	"github.com/zafir-co-ao/onna-narciso/internal/shared/event"
 	"github.com/zafir-co-ao/onna-narciso/web"
 )
 
 func main() {
+	bus := event.NewInmemEventBus()
 	repo := inmem.NewAppointmentRepository()
 	cacl := stubs.CustomerAclStub{}
 	pacl := stubs.Pacl
 	sacl := stubs.Sacl
 
-	s := scheduling.NewAppointmentScheduler(repo, cacl, pacl, sacl)
-	c := scheduling.NewAppointmentCanceler(repo)
+	s := scheduling.NewAppointmentScheduler(repo, cacl, pacl, sacl, bus)
+	c := scheduling.NewAppointmentCanceler(repo, bus)
 	f := scheduling.NewAppointmentFinder(repo)
-	r := scheduling.NewAppointmentRescheduler(repo)
+	r := scheduling.NewAppointmentRescheduler(repo, bus)
 
 	http.Handle("/", web.NewRouter(s, c, f, r))
 
