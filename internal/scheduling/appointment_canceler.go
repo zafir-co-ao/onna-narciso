53f427009ab5a656133493b889d000453dd24390
jocaetano1 <josan5368@gmail.com>
2024-10-29 16:22:13 +0100

    test: check if event appointment_canceled was published

--- internal/scheduling/appointment_canceler.go
+++ internal/scheduling/appointment_canceler.go
@@ -1,15 +1,20 @@
 package scheduling
 
+import "github.com/zafir-co-ao/onna-narciso/internal/shared/event"
+
+const EventAppointmentCanceled = "EventAppointmentCanceled"
+
 type AppointmentCanceler interface {
 	Execute(id string) error
 }
 
 type appointmentCancelerImpl struct {
 	repo AppointmentRepository
+	bus  event.Bus
 }
 
-func NewAppointmentCanceler(repo AppointmentRepository) AppointmentCanceler {
-	return &appointmentCancelerImpl{repo}
+func NewAppointmentCanceler(repo AppointmentRepository, bus event.Bus) AppointmentCanceler {
+	return &appointmentCancelerImpl{repo, bus}
 }
 
 func (u *appointmentCancelerImpl) Execute(id string) error {
@@ -24,6 +29,9 @@ func (u *appointmentCancelerImpl) Execute(id string) error {
 	}
 
 	u.repo.Save(a)
+
+	e := event.New(EventAppointmentCanceled)
+	u.bus.Publish(e)
 
 	return nil
 }
--- internal/scheduling/appointment_canceler_test.go
+++ internal/scheduling/appointment_canceler_test.go
@@ -6,18 +6,23 @@ import (
 
 	"github.com/zafir-co-ao/onna-narciso/internal/scheduling"
 	"github.com/zafir-co-ao/onna-narciso/internal/scheduling/adapters/inmem"
+	"github.com/zafir-co-ao/onna-narciso/internal/shared/event"
 )
 
 func TestAppointmentCanceler(t *testing.T) {
 	repo := inmem.NewAppointmentRepository()
+	bus := event.NewInmemEventBus()
+
 	a1 := scheduling.Appointment{ID: "1"}
 	a2 := scheduling.Appointment{ID: "2", Status: scheduling.StatusCanceled}
+	a3 := scheduling.Appointment{ID: "3"}
 
 	repo.Save(a1)
 	repo.Save(a2)
+	repo.Save(a3)
 
 	t.Run("should_cancel_an_appointment", func(t *testing.T) {
-		usecase := scheduling.NewAppointmentCanceler(repo)
+		usecase := scheduling.NewAppointmentCanceler(repo, bus)
 
 		err := usecase.Execute("1")
 		if err != nil {
@@ -35,7 +40,7 @@ func TestAppointmentCanceler(t *testing.T) {
 	})
 
 	t.Run("should_return_error_if_appointment_status_is_canceled", func(t *testing.T) {
-		usecase := scheduling.NewAppointmentCanceler(repo)
+		usecase := scheduling.NewAppointmentCanceler(repo, bus)
 
 		err := usecase.Execute("2")
 		if err == nil {
@@ -48,9 +53,9 @@ func TestAppointmentCanceler(t *testing.T) {
 	})
 
 	t.Run("should_return_error_appointment_not_found_when_appointment_not_exists_in_repository", func(t *testing.T) {
-		usecase := scheduling.NewAppointmentCanceler(repo)
+		usecase := scheduling.NewAppointmentCanceler(repo, bus)
 
-		err := usecase.Execute("3")
+		err := usecase.Execute("1000")
 		if err == nil {
 			t.Errorf("Canceling appointment should return error: %v", err)
 		}
@@ -59,4 +64,39 @@ func TestAppointmentCanceler(t *testing.T) {
 			t.Errorf("Canceling appointment should return error: %v", err)
 		}
 	})
+
+	t.Run("must_publish_the_canceled_appointment_event", func(t *testing.T) {
+		h := &fakeStorageHandler{}
+		bus := event.NewInmemEventBus()
+		bus.Subscribe(scheduling.EventAppointmentCanceled, h)
+
+		usecase := scheduling.NewAppointmentCanceler(repo, bus)
+
+		err := usecase.Execute("3")
+		if !errors.Is(nil, err) {
+			t.Errorf("Should not return an error, got %v", err)
+		}
+
+		if !h.WasPublished(scheduling.EventAppointmentCanceled) {
+			t.Error("The EventAppointmentCanceled must be published")
+		}
+	})
+}
+
+type fakeStorageHandler struct {
+	events []event.Event
+}
+
+func (s *fakeStorageHandler) Handle(e event.Event) {
+	s.events = append(s.events, e)
+}
+
+func (s *fakeStorageHandler) WasPublished(name string) bool {
+	for _, e := range s.events {
+		if e.Name() == name {
+			return true
+		}
+	}
+
+	return false
 }
