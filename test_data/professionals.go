fcc3c3368c993ccb3ce602167e671efce2b782ba
Dzerjinski Lemos <dzerjinski.lemos@zafir.co.ao>
2024-11-01 19:12:39 +0100

    sex 01 nov 2024 19:12:39 WAT

---
+++ test_data/professionals.go
@@ -0,0 +1,13 @@
+package testdata
+
+import (
+	"github.com/zafir-co-ao/onna-narciso/internal/scheduling"
+	"github.com/zafir-co-ao/onna-narciso/internal/shared/id"
+)
+
+var Professionals []scheduling.Professional = []scheduling.Professional{
+	{ID: "1", Name: "Joana D'Arc", ServicesIDS: []id.ID{"1", "2"}},
+	{ID: "2", Name: "Napoléon Bonaparte", ServicesIDS: []id.ID{"3"}},
+	{ID: "3", Name: "Alexandre III", ServicesIDS: []id.ID{"4"}},
+	{ID: "4", Name: "Cleopatra", ServicesIDS: []id.ID{"1", "2", "3", "4"}},
+}
