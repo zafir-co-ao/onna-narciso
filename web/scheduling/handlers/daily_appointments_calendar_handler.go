5b1d9c928f6e806ae0cdd380864b31fee836d893
jocaetano1 <josan5368@gmail.com>
2024-11-14 09:22:12 +0100

    refactor: adjustments

--- web/scheduling/handlers/daily_appointments_calendar_handler.go
+++ web/scheduling/handlers/daily_appointments_calendar_handler.go
@@ -11,7 +11,7 @@ import (
 func HandleDailyAppointmentsCalendar() func(w http.ResponseWriter, r *http.Request) {
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		value := r.FormValue("operation")
+		operation := r.FormValue("operation")
 		state := components.CalendarState{
 			HxGet:     r.FormValue("hx-get"),
 			HxTarget:  r.FormValue("hx-target"),
@@ -25,12 +25,12 @@ func HandleDailyAppointmentsCalendar() func(w http.ResponseWriter, r *http.Reque
 			components.Calendar(state).Render(r.Context(), w)
 		}
 
-		if value == "previous-month" {
+		if operation == "previous-month" {
 			date = date.AddDate(0, -1, 0)
 			state.Date = date.Format("2006-01-02")
 		}
 
-		if value == "next-month" {
+		if operation == "next-month" {
 			date = date.AddDate(0, 1, 0)
 			state.Date = date.Format("2006-01-02")
 		}
