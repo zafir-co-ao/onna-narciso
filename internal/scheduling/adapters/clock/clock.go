b165fe6c22116402727d04d3827143215a22c7a1
jocaetano1 <josan5368@gmail.com>
2024-11-28 18:28:19 +0100

    refactor: adjustments

--- internal/scheduling/adapters/clock/clock.go
+++ internal/scheduling/adapters/clock/clock.go
@@ -1,12 +1,12 @@
-package stubs
+package clock
 
 import (
 	"github.com/zafir-co-ao/onna-narciso/internal/scheduling"
 	"github.com/zafir-co-ao/onna-narciso/internal/shared/date"
 )
 
-func NewClock() scheduling.ClockFunc {
+func New() scheduling.ClockFunc {
 	return func() date.Date {
-		return date.Date("2016-01-01")
+		return date.Today()
 	}
 }
