a6fa2dd9f8e8e8386b168152c1ed2d84875615ab
Dzerjinski Lemos <dzerjinski.lemos@zafir.co.ao>
2024-11-28 17:33:12 +0100

    feat: add notifications feature

---
+++ internal/notifications/adapters/twillio_service.go
@@ -0,0 +1,54 @@
+package adapters
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/kindalus/godx/pkg/assert"
+	"github.com/twilio/twilio-go"
+	api "github.com/twilio/twilio-go/rest/api/v2010"
+	"github.com/zafir-co-ao/onna-narciso/internal/notifications"
+)
+
+type notifierFunc func(n, msg string) error
+
+func (f notifierFunc) Notify(n, msg string) error {
+	return f(n, msg)
+}
+
+func NewTwillioSMSNotifier() notifications.Notifier {
+	return notifierFunc(
+
+		func(n, msg string) error {
+
+			// Find your Account SID and Auth Token at twilio.com/console
+			// and set the environment variables. See http://twil.io/secure
+			// Make sure TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_NUMBER exists in your environment
+			client := twilio.NewRestClient()
+
+			assert.NotNil(os.Getenv("TWILIO_ACCOUNT_SID"), "TWILIO_ACCOUNT_SID env not found")
+			assert.NotNil(os.Getenv("TWILIO_AUTH_TOKEN"), "TWILIO_AUTH_TOKEN env not found")
+			assert.NotNil(os.Getenv("TWILIO_NUMBER"), "TWILIO_NUMBER env not found")
+
+			from := os.Getenv("TWILIO_NUMBER")
+
+			params := &api.CreateMessageParams{}
+			params.SetBody(msg)
+			params.SetFrom(from)
+			//params.SetFrom("+15017122661")
+			params.SetTo(n)
+
+			resp, err := client.Api.CreateMessage(params)
+			if err != nil {
+				slog.Error(err.Error())
+				return err
+			}
+
+			if resp.Body != nil {
+				slog.Info(*resp.Body)
+			}
+
+			return nil
+		},
+	)
+}
