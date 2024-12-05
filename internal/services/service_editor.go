989e5e85a088ce1a58a4c62c6781ebf216cbf03f
novaisdavid <novaisdavid.19@gmail.com>
2024-12-05 12:20:15 +0100

    test: should_edit_description_of_service

--- internal/services/service_editor.go
+++ internal/services/service_editor.go
@@ -1,46 +1,76 @@
 package services
 
 import (
+	"github.com/kindalus/godx/pkg/event"
 	"github.com/kindalus/godx/pkg/nanoid"
 	"github.com/zafir-co-ao/onna-narciso/internal/services/price"
+	"github.com/zafir-co-ao/onna-narciso/internal/shared/duration"
 	"github.com/zafir-co-ao/onna-narciso/internal/shared/name"
 )
 
+const EventServiceEdited = "EventServiceEdited"
+
+type ServiceEditorInput struct {
+	ID          string
+	Name        string
+	Description string
+	Price       string
+	Duration    int
+}
+
 type ServiceEditor interface {
-	Edit(id nanoid.ID, name, newPrice string) error
+	Edit(i ServiceEditorInput) error
 }
 
 type ServiceEditorImpl struct {
 	repo Repository
+	bus  event.Bus
 }
 
-func NewServiceEditor(repo Repository) ServiceEditor {
-	return &ServiceEditorImpl{repo: repo}
+func NewServiceEditor(repo Repository, bus event.Bus) ServiceEditor {
+	return &ServiceEditorImpl{repo: repo, bus: bus}
 }
 
-func (u *ServiceEditorImpl) Edit(i nanoid.ID, newName string, newPrice string) error {
+func (u *ServiceEditorImpl) Edit(i ServiceEditorInput) error {
 
-	_, err := u.repo.FindByID(i)
+	_, err := u.repo.FindByID(nanoid.ID(i.ID))
 	if err != nil {
 		return err
 	}
 
-	_name, err := name.New(newName)
+	_name, err := name.New(i.Name)
 	if err != nil {
 		return err
 	}
 
-	_price, err := price.New(newPrice)
+	_price, err := price.New(i.Price)
+	if err != nil {
+		return err
+	}
 
-	s := NewServiceBuilder().WithID(i).
+	_duration, err := duration.New(i.Duration)
+	if err != nil {
+		return err
+	}
+
+	s := NewServiceBuilder().WithID(nanoid.ID(i.ID)).
 		WithName(_name).
 		WithPrice(_price).
+		WithDuration(_duration).
 		Build()
 
 	err = u.repo.Save(s)
 	if err != nil {
 		return err
 	}
+
+	e := event.New(
+		EventServiceEdited,
+		event.WithHeader(event.HeaderAggregateID, s.ID.String()),
+		event.WithPayload(i),
+	)
+
+	u.bus.Publish(e)
 
 	return nil
 }
--- internal/services/service_editor_test.go
+++ internal/services/service_editor_test.go
@@ -5,34 +5,37 @@ import (
 	"testing"
 
 	"github.com/kindalus/godx/pkg/event"
-	"github.com/kindalus/godx/pkg/nanoid"
 	"github.com/zafir-co-ao/onna-narciso/internal/services"
 	"github.com/zafir-co-ao/onna-narciso/internal/services/adapters/inmem"
 )
 
 func TestServiceEdit(t *testing.T) {
+	bus := event.NewEventBus()
+	repo := inmem.NewServiceRepository()
+	u := services.NewServiceCreator(repo, bus)
+	e := services.NewServiceEditor(repo, bus)
+
+	i := services.ServiceCreatorInput{
+		Name:     "Manicure",
+		Price:    "1000",
+		Duration: 60,
+	}
 
 	t.Run("should_recovery_service_with_id", func(t *testing.T) {
-		bus := event.NewEventBus()
-		repo := inmem.NewServiceRepository()
-		u := services.NewServiceCreator(repo, bus)
-		e := services.NewServiceEditor(repo)
-
-		i := services.ServiceCreatorInput{
-			Name:     "Manicure",
-			Price:    "1000",
-			Duration: 60,
-		}
-
 		o, err := u.Create(i)
 
 		if !errors.Is(nil, err) {
 			t.Errorf("Expected no error, got %v", err)
 		}
-		newName := "Pedicure"
-		newPrice := "1500"
 
-		err = e.Edit(nanoid.ID(o.ID), newName, newPrice)
+		i := services.ServiceEditorInput{
+			ID:       o.ID,
+			Name:     "Manicure e Pedicure",
+			Price:    "1500",
+			Duration: 90,
+		}
+
+		err = e.Edit(i)
 
 		if !errors.Is(nil, err) {
 			t.Errorf("Expected no error, got %v", err)
@@ -41,27 +44,20 @@ func TestServiceEdit(t *testing.T) {
 	})
 
 	t.Run("should_edit_name_of_service", func(t *testing.T) {
-		bus := event.NewEventBus()
-		repo := inmem.NewServiceRepository()
-		u := services.NewServiceCreator(repo, bus)
-		e := services.NewServiceEditor(repo)
-
-		i := services.ServiceCreatorInput{
-			Name:     "Manicure",
-			Price:    "1000",
-			Duration: 60,
-		}
-
 		o, err := u.Create(i)
 
 		if !errors.Is(nil, err) {
 			t.Errorf("Expected no error, got %v", err)
 		}
 
-		newName := "Manicure e Pedicure"
-		newPrice := "1500"
+		i := services.ServiceEditorInput{
+			ID:       o.ID,
+			Name:     "Manicure e Pedicure",
+			Price:    "1500",
+			Duration: 90,
+		}
 
-		err = e.Edit(nanoid.ID(o.ID), newName, newPrice)
+		err = e.Edit(i)
 
 		if !errors.Is(nil, err) {
 			t.Errorf("Expected no error, got %v", err)
@@ -69,15 +65,54 @@ func TestServiceEdit(t *testing.T) {
 	})
 
 	t.Run("should_edit_price_of_service", func(t *testing.T) {
-		bus := event.NewEventBus()
-		repo := inmem.NewServiceRepository()
-		u := services.NewServiceCreator(repo, bus)
-		e := services.NewServiceEditor(repo)
+		o, err := u.Create(i)
 
-		i := services.ServiceCreatorInput{
-			Name:     "Manicure",
-			Price:    "1000",
-			Duration: 60,
+		if !errors.Is(nil, err) {
+			t.Errorf("Expected no error, got %v", err)
+		}
+
+		i := services.ServiceEditorInput{
+			ID:       o.ID,
+			Name:     "Manicure e Pedicure",
+			Price:    "1500",
+			Duration: 90,
+		}
+
+		err = e.Edit(i)
+
+		if !errors.Is(nil, err) {
+			t.Errorf("Expected no error, got %v", err)
+		}
+	})
+
+	t.Run("should_edit_description_of_service", func(t *testing.T) {
+		o, err := u.Create(i)
+
+		if !errors.Is(nil, err) {
+			t.Errorf("Expected no error, got %v", err)
+		}
+
+		i := services.ServiceEditorInput{
+			ID:          o.ID,
+			Name:        "Manicure e Pedicure",
+			Price:       "1500",
+			Description: "Com gelinho na no pé",
+			Duration:    90,
+		}
+
+		err = e.Edit(i)
+
+		if !errors.Is(nil, err) {
+			t.Errorf("Expected no error, got %v", err)
+		}
+	})
+
+	t.Run("should_publish_the_domain_event_when_service_is_edited", func(t *testing.T) {
+		isPublished := false
+		var h event.HandlerFunc = func(e event.Event) {
+			if e.Name() == services.EventServiceEdited {
+				isPublished = true
+			}
 		}
 
 		o, err := u.Create(i)
@@ -86,13 +121,23 @@ func TestServiceEdit(t *testing.T) {
 			t.Errorf("Expected no error, got %v", err)
 		}
 
-		newName := "Manicure e Pedicure"
-		newprice := "1500"
+		i := services.ServiceEditorInput{
+			ID:          o.ID,
+			Name:        "Manicure e Pedicure",
+			Price:       "1500",
+			Description: "Com gelinho na no pé",
+			Duration:    90,
+		}
 
-		err = e.Edit(nanoid.ID(o.ID), newName, newprice)
+		bus.Subscribe(services.EventServiceEdited, h)
 
+		err = e.Edit(i)
 		if !errors.Is(nil, err) {
 			t.Errorf("Expected no error, got %v", err)
+		}
+
+		if !isPublished {
+			t.Errorf("The %v should be published", services.EventServiceEdited)
 		}
 	})
 
