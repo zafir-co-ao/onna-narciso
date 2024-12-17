fe5d090d0220a699cf2d15fc02773edeaa749b1f
jocaetano1 <josan5368@gmail.com>
2024-12-17 18:55:23 +0100

    feat: implementation of hr_service_acl in scheduling

--- internal/scheduling/hr_service_acl.go
+++ internal/scheduling/hr_service_acl.go
@@ -4,18 +4,39 @@ import (
 	"errors"
 
 	"github.com/kindalus/godx/pkg/nanoid"
+	"github.com/zafir-co-ao/onna-narciso/internal/hr"
+	"github.com/zafir-co-ao/onna-narciso/internal/shared/name"
 )
 
-var (
-	ErrProfessionalNotFound = errors.New("professional not found")
-)
+var ErrProfessionalNotFound = errors.New("professional not found")
 
-type ProfessionalsACL interface {
+type HRServiceACL interface {
 	FindProfessionalByID(id nanoid.ID) (Professional, error)
 }
 
-type ProfessionalsACLFunc func(id nanoid.ID) (Professional, error)
+type HRServiceACLFunc func(id nanoid.ID) (Professional, error)
 
-func (f ProfessionalsACLFunc) FindProfessionalByID(id nanoid.ID) (Professional, error) {
+func (f HRServiceACLFunc) FindProfessionalByID(id nanoid.ID) (Professional, error) {
 	return f(id)
+}
+
+type internalHRServiceACL struct {
+	finder hr.ProfessionalFinder
+}
+
+func NewInternalHRServiceACL(finder hr.ProfessionalFinder) HRServiceACL {
+	return &internalHRServiceACL{finder}
+}
+
+func (i *internalHRServiceACL) FindProfessionalByID(id nanoid.ID) (Professional, error) {
+	p, err := i.finder.FindByID(id.String())
+
+	if err != nil {
+		return Professional{}, ErrProfessionalNotFound
+	}
+
+	return Professional{
+		ID:   nanoid.ID(p.ID),
+		Name: name.Name(p.Name),
+	}, nil
 }
