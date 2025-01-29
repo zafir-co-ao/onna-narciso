6002a7ae03942c491f37d3860e849a285d445771
silvano-paulino <silvanocavimbi20@gmail.com>
2025-01-29 14:02:22 +0100

    add tests to send email and validate email

--- internal/auth/user_password_resetter.go
+++ internal/auth/user_password_resetter.go
@@ -1,5 +1,12 @@
 package auth
 
+import (
+	"fmt"
+
+	"github.com/kindalus/godx/pkg/event"
+	"github.com/zafir-co-ao/onna-narciso/internal/notifications"
+)
+
 type UserPasswordResetter interface {
 	Reset(i UserPasswordResetterInput) error
 }
@@ -8,16 +15,25 @@ type UserPasswordResetterInput struct {
 	Email string
 }
 
-func NewUserPasswordResetter(r Repository) UserPasswordResetter {
-	return &userPasswordResetterImpl{r}
+const EventUserPasswordReset = "EventUserPasswordReset"
+
+func NewUserPasswordResetter(r Repository, bus event.Bus, n notifications.Notifier) UserPasswordResetter {
+	return &userPasswordResetterImpl{repo: r, bus: bus, notifier: n}
 }
 
 type userPasswordResetterImpl struct {
-	repo Repository
+	repo     Repository
+	bus      event.Bus
+	notifier notifications.Notifier
 }
 
 func (u *userPasswordResetterImpl) Reset(i UserPasswordResetterInput) error {
-	user, err := u.repo.FindByEmail(Email(i.Email))
+	email, err := NewEmail(i.Email)
+	if err != nil {
+		return err
+	}
+
+	user, err := u.repo.FindByEmail(email)
 	if err != nil {
 		return err
 	}
@@ -29,7 +45,36 @@ func (u *userPasswordResetterImpl) Reset(i UserPasswordResetterInput) error {
 
 	user.ResetPassword(p)
 
-	err = u.repo.Save(user)
+	if err := u.repo.Save(user); err != nil {
+		return err
+	}
+
+	if err = u.SentPasswordToEmail(user, p); err != nil {
+		return err
+	}
+
+	e := event.New(EventUserPasswordReset,
+		event.WithHeader(event.HeaderAggregateID, user.ID.String()),
+		event.WithPayload(i),
+	)
+
+	u.bus.Publish(e)
+
+	return nil
+}
+
+func (u *userPasswordResetterImpl) SentPasswordToEmail(user User, p string) error {
+	c := notifications.Contact{
+		Name:  user.Username.String(),
+		Email: user.Email.String(),
+	}
+
+	msg := notifications.Message{
+		Subject: "Nova palavra-passe",
+		Body:    fmt.Sprintf("A sua nova palavra-passe é: %s", p),
+	}
+
+	err := u.notifier.Notify(c, msg)
 	if err != nil {
 		return err
 	}
--- internal/auth/user_password_resetter_test.go
+++ internal/auth/user_password_resetter_test.go
@@ -2,15 +2,21 @@ package auth_test
 
 import (
 	"errors"
+	"strings"
 	"testing"
 
+	"github.com/kindalus/godx/pkg/event"
 	"github.com/zafir-co-ao/onna-narciso/internal/auth"
+	"github.com/zafir-co-ao/onna-narciso/internal/auth/stubs"
 )
 
 func TestUserPasswordResetter(t *testing.T) {
 	user := auth.User{Email: "kate@gmail.com", Password: auth.MustNewPassword("kate1234")}
 	repo := auth.NewInmemRepository(user)
-	u := auth.NewUserPasswordResetter(repo)
+	nStub := stubs.NewNoticationsStub()
+	bus := event.NewEventBus()
+
+	u := auth.NewUserPasswordResetter(repo, bus, nStub)
 	i := auth.UserPasswordResetterInput{Email: "kate@gmail.com"}
 
 	t.Run("should_retrieve_user_in_repository", func(t *testing.T) {
@@ -44,6 +50,68 @@ func TestUserPasswordResetter(t *testing.T) {
 	})
 
 	t.Run("should_send_new_password_to_email", func(t *testing.T) {
-		
+		err := u.Reset(i)
+
+		if !errors.Is(nil, err) {
+			t.Errorf("Expected no error, got %v", err)
+		}
+
+		if nStub.Contact.Email != user.Email.String() {
+			t.Errorf("Expected email to be sent to %s, got %v", user.Email.String(), nStub.Contact.Email)
+		}
+
+		expectedPrefix := "A sua nova palavra-passe é:"
+		if !strings.HasPrefix(nStub.Message.Body, expectedPrefix) {
+			t.Errorf("Expected email body to contain new password prefix, got %s", nStub.Message.Body)
+		}
+	})
+
+	t.Run("should_publish_event_when_user_password_is_reset", func(t *testing.T) {
+		isPublished := false
+
+		var h event.HandlerFunc = func(e event.Event) {
+			if e.Name() == auth.EventUserPasswordReset {
+				isPublished = true
+			}
+		}
+		bus.Subscribe(auth.EventUserPasswordReset, h)
+
+		err := u.Reset(i)
+
+		if !errors.Is(nil, err) {
+			t.Errorf("Expected no error, got %v", err)
+		}
+
+		if !isPublished {
+			t.Errorf("The %s must be published", auth.EventUserPasswordReset)
+		}
+	})
+
+	t.Run("should_return_error_if_email_is_empty", func(t *testing.T) {
+		i := auth.UserPasswordResetterInput{Email: ""}
+
+		err := u.Reset(i)
+
+		if errors.Is(nil, err) {
+			t.Errorf("Expected no error, got %v", err)
+		}
+
+		if !errors.Is(err, auth.ErrEmptyEmail) {
+			t.Errorf("The error must be %v, got %v", auth.ErrEmptyEmail, err)
+		}
+	})
+
+	t.Run("should_return_error_if_email_is_in_invalid_format", func(t *testing.T) {
+		i := auth.UserPasswordResetterInput{Email: "kate@123"}
+
+		err := u.Reset(i)
+
+		if errors.Is(nil, err) {
+			t.Errorf("Expected no error, got %v", err)
+		}
+
+		if !errors.Is(err, auth.ErrInvalidEmailFormat) {
+			t.Errorf("The error must be %v, got %v", auth.ErrInvalidEmailFormat, err)
+		}
 	})
 }
